d564bf831fa705ac32d5c000c4342cfec0dfe973
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-22 21:13:33 +0100

    Update essaisessais.py

--- essaisessais.py
+++ essaisessais.py
@@ -39,11 +39,6 @@ for (x,y,w,h) in faces:
 im = Image.open("10020_5.png")
 pix = im.load()
 
-b = pix[100,200]
-#print(b)
-
-
-
 
 dico = {}
 for value in im.getdata():
@@ -51,7 +46,7 @@ for value in im.getdata():
          dico[value] += 1
     else:
          dico[value] = 1
-
+print("oui")
 
 
 liste_dico = []
@@ -64,37 +59,45 @@ liste_dico= sorted(liste_dico, reverse = True)
 
 c = 0
 liste_couleur = []
+
 for i in liste_dico:
     liste_couleur.append([c for c,v in dico.items() if v==i])
-    if i < 100:
+    c+=1
+    if c == 16:
         break
 
-print(liste_couleur)
+
+
+
+
+    
+
 
 im = cv2.imread("10025_6.png")
 
-#im[200:300, 50] = [255,255,255]
-
+#ouille lui on voit qu'il fait de grosse journée ^^
+#ouais bah...
+#bordel que c moche ^^
 
 for x in range(im.shape[0]):
     for y in range(im.shape[1]):
         
-        if im[x,y][0] != 121 and im[x,y][1] != 125 and im[x,y][2] != 98 and\
-           im[x,y][0] != 0 and im[x,y][1] != 0 and im[x,y][2] != 255 and\
-           im[x,y][0] != 119 and im[x,y][1] != 123 and im[x,y][2] != 96 and\
-           im[x,y][0] != 122 and im[x,y][1] != 126 and im[x,y][2] != 99 and\
-           im[x,y][0] != 101 and im[x,y][1] != 105 and im[x,y][2] != 76 and\
-           im[x,y][0] != 117 and im[x,y][1] != 121 and im[x,y][2] != 94 and\
-           im[x,y][0] != 118 and im[x,y][1] != 122 and im[x,y][2] != 95 and\
-           im[x,y][0] != 117 and im[x,y][1] != 123 and im[x,y][2] != 96 and\
-           im[x,y][0] != 122 and im[x,y][1] != 125 and im[x,y][2] != 101 and\
-           im[x,y][0] != 122 and im[x,y][1] != 126 and im[x,y][2] != 97 and\
-           im[x,y][0] != 231 and im[x,y][1] != 208 and im[x,y][2] != 166 and\
-           im[x,y][0] != 110 and im[x,y][1] != 114 and im[x,y][2] != 87 and\
-           im[x,y][0] != 110 and im[x,y][1] != 114 and im[x,y][2] != 87 and\
-           im[x,y][0] != 123 and im[x,y][1] != 125 and im[x,y][2] != 97 and\
-           im[x,y][0] != 224 and im[x,y][1] != 197 and im[x,y][2] != 150 and\
-           im[x,y][0] != 240 and im[x,y][1] != 219 and im[x,y][2] != 178 :
+        if im[x,y][0] != liste_couleur[0][0][0] and im[x,y][1] != liste_couleur[0][0][1] and im[x,y][2] != liste_couleur[0][0][2] and\
+           im[x,y][0] != liste_couleur[1][0][0] and im[x,y][1] != liste_couleur[1][0][1] and im[x,y][2] != liste_couleur[1][0][2] and\
+           im[x,y][0] != liste_couleur[2][0][0] and im[x,y][1] != liste_couleur[2][0][1] and im[x,y][2] != liste_couleur[2][0][2] and\
+           im[x,y][0] != liste_couleur[3][0][0] and im[x,y][1] != liste_couleur[3][0][1] and im[x,y][2] != liste_couleur[3][0][2] and\
+           im[x,y][0] != liste_couleur[4][0][0] and im[x,y][1] != liste_couleur[4][0][1] and im[x,y][2] != liste_couleur[4][0][2] and\
+           im[x,y][0] != liste_couleur[5][0][0] and im[x,y][1] != liste_couleur[5][0][1] and im[x,y][2] != liste_couleur[5][0][2] and\
+           im[x,y][0] != liste_couleur[6][0][0] and im[x,y][1] != liste_couleur[6][0][1] and im[x,y][2] != liste_couleur[6][0][2] and\
+           im[x,y][0] != liste_couleur[7][0][0] and im[x,y][1] != liste_couleur[7][0][1] and im[x,y][2] != liste_couleur[7][0][2] and\
+           im[x,y][0] != liste_couleur[8][0][0] and im[x,y][1] != liste_couleur[8][0][1] and im[x,y][2] != liste_couleur[8][0][2] and\
+           im[x,y][0] != liste_couleur[9][0][0] and im[x,y][1] != liste_couleur[9][0][1] and im[x,y][2] != liste_couleur[9][0][2] and\
+           im[x,y][0] != liste_couleur[10][0][0] and im[x,y][1] != liste_couleur[10][0][1] and im[x,y][2] != liste_couleur[10][0][2] and\
+           im[x,y][0] != liste_couleur[11][0][0] and im[x,y][1] != liste_couleur[11][0][1] and im[x,y][2] != liste_couleur[11][0][2] and\
+           im[x,y][0] != liste_couleur[12][0][0] and im[x,y][1] != liste_couleur[12][0][1] and im[x,y][2] != liste_couleur[12][0][2] and\
+           im[x,y][0] != liste_couleur[13][0][0] and im[x,y][1] != liste_couleur[13][0][1] and im[x,y][2] != liste_couleur[13][0][2] and\
+           im[x,y][0] != liste_couleur[14][0][0] and im[x,y][1] != liste_couleur[14][0][1] and im[x,y][2] != liste_couleur[14][0][2] and\
+           im[x,y][0] != liste_couleur[15][0][0] and im[x,y][1] != liste_couleur[15][0][1] and im[x,y][2] != liste_couleur[15][0][2]:
            
            
             im[x,y] = [0,0,0]
@@ -122,22 +125,22 @@ cv2.imshow("jojo.png", im)
 
 
 
-    def only_tete_sans_cheveux(self):
-        img = cv2.imread('[601 126 289 289].png')
+def only_tete_sans_cheveux(self):
+    img = cv2.imread('[601 126 289 289].png')
 
 
-        mask = np.zeros(img.shape[:2],np.uint8)
+    mask = np.zeros(img.shape[:2],np.uint8)
 
-        bgdModel = np.zeros((1,65),np.float64)
-        fgdModel = np.zeros((1,65),np.float64)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
 
-        rect = (50,50,450,290)
-        cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+    rect = (50,50,450,290)
+    cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 
-        mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-        img = img*mask2[:,:,np.newaxis]
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    img = img*mask2[:,:,np.newaxis]
 
-        plt.imshow(img),plt.colorbar(),plt.show()
+    plt.imshow(img),plt.colorbar(),plt.show()
 
 
 
@@ -158,7 +161,11 @@ def countour(self):
     cv2.destroyAllWindows()
 
 
-        
+#1-extraire le corps
+#2-extraire les couleurs de la peau de la tete
+#3-faire la fonction
+#4-find coutour
+#5-apres ben j'en sais rien putin oublie pas t'as le truk pts centré round main faut extraire LA MAIN    
  
 
 
@@ -171,10 +178,6 @@ def countour(self):
 
 
 
-
-
-
-#ok ca marche mais jarrive pas a faire la fonction putin
 
 
 
