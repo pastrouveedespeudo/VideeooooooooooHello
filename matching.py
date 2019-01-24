68a6b72cad35f93077a7fd0f847e4fc840065864
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-24 19:35:50 +0100

    Update matching.py

--- matching.py
+++ matching.py
@@ -1,27 +1,45 @@
+import os
+
 class match:
 
-    def match(self):
+    def match_pouce(self, path1, path2):
+
+        self.path1 = path1
+        self.path2 = path2
         
-        img1 = cv2.imread ("bouboule1.png", 0)
-        img2 = cv2.imread ("bouboule2.png", 0)
+        os.chdir(self.path1)
+        
+        liste1 = os.listdir()
+        for i in liste1:
+            shutil.move(i, self.path2)        
 
-        orb = cv2.ORB_create()
+        os.chdir(self.path2)
+        liste2 = os.listdir(self.path2)
+        
+        for i in liste1:
 
-        kp1, des1 = orb.detectAndCompute(img1,None)
-        kp2, des2 = orb.detectAndCompute(img2,None)
+            cv2.imread(i)
+            cv2.imread(liste2[-1])
 
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(des1,des2)
+            orb = cv2.ORB_create()
 
-        matches = sorted(matches, key = lambda x:x.distance)
+            kp1, des1 = orb.detectAndCompute(img1,None)
+            kp2, des2 = orb.detectAndCompute(img2,None)
 
-        c = 0
-        for match in matches:
-            c+=1
-        if c > 100:
-                print(c)
-                img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:50],None, flags=2)
-                plt.imshow(img3)
-                plt.show()
+            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+            matches = bf.match(des1,des2)
 
-        c = 0
+            matches = sorted(matches, key = lambda x:x.distance)
+
+            c = 0
+            for match in matches:
+                c+=1
+                
+            if c > 100:
+                print("y'a eu un pouce")
+                #marquer sur la video
+
+            c = 0
+
+        for i in liste1:
+            shutil.move(i, self.path1)     
