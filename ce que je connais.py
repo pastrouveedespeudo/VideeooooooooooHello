b1250ce02af76edd5a5f5f9232808025597ed752
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-25 19:38:53 +0100

    Add files via upload

---
+++ ce que je connais.py
@@ -0,0 +1,121 @@
+#tete/crane
+
+#geste visage:
+
+#cou
+
+#epaul
+
+
+#buste
+
+
+#bide
+
+
+#bras
+
+
+#avant bras
+
+
+#poigné
+
+
+#coude
+
+
+#hanche
+
+
+#bassin
+
+
+#cuisse
+
+
+#genoux
+
+
+
+#tibia
+
+
+#patte
+
+
+#position des yeux // psy
+
+#habit:
+#couleur
+#style
+#lunette
+#lunette ou pas ? couleur, forme (attention la vendeuse a hard influé dessus
+#                       aussi selon sa perception)
+
+
+#coupes de cheveux:
+#la aussi la coiffeuse a fais selon son style et de comment elle l'a percu
+
+#bijous:
+#pouce
+#majeur
+#index
+#celui entre les deux
+#petit dois annuaire
+#oreille
+#collier
+
+
+#prenom // note
+
+
+
+#boutons
+
+
+#ride
+
+
+#coupe de cheveux
+
+
+#carac du corps:
+#bouche
+#doigt
+
+#passé:
+#carac psycho
+
+
+
+#les choses en accord:
+#habits
+
+#prosodie
+
+
+
+
+
+
+#les contextes: (que je connais L, S, interim, sportif, cuisine)
+#micro ? macro ? famille? amis ? inconnue ? position hiearchique ? age ?travail?
+#heure? 8/10/12/14/17/20/22
+#météo
+
+
+#eve A -> b -> c
+
+#joie automatiquement baissera de suite ou dans les prochains ex: 18/20 la meuf
+#2 personnes se rencontres -> soit hiarchie 1 ok ou les deux pas copains
+#                           soit copain
+#   
+#emotion // engagement ect
+
+
+
+
+
+
+
